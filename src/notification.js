f5bfdbef4b648a5cacf2418cb6a1b5cabdad50ef
Frederic Junod <frederic.junod@camptocamp.com>
2018-11-25 16:09:32 +0100

    Manage service workers

--- src/notification.js
+++ src/notification.js
@@ -2,19 +2,19 @@ export default class Notifier {
 
   constructor(actionHandler) {
 
-    Notification.requestPermission().then((result) => {
-      if (result === 'granted') {
-        navigator.serviceWorker.register('service-worker.js').then((registration) => {
-          this.registration = registration;
-        });
-
-      }
-    });
-
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      if (event.data.action) {
-        actionHandler(event.data);
-      }
+    navigator.serviceWorker.ready.then((registration) => {
+      Notification.requestPermission().then((result) => {
+        if (result === 'granted') {
+          navigator.serviceWorker.register('service-worker.notification.js').then((registration) => {
+            this.registration = registration;
+          });
+        }
+      });
+      navigator.serviceWorker.addEventListener('message', (event) => {
+        if (event.data.action) {
+          actionHandler(event.data);
+        }
+      });
     });
   }
 
